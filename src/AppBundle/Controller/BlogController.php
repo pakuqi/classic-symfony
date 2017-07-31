d8fab593e02408f13ee54eaeaf0523d010dc03d0
Toru Ohno <daye001@gmail.com>
2017-07-31 23:07:39 +0900

    5-4 ブログ記事一覧

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -9,20 +9,27 @@ class BlogController extends Controller
 {
 
   public function latestListAction() {
-    $blogList = [
-      [
-        'targetDate' => '2015年3月15日',
-        'title' => '東京公演レポート',
-      ],
-      [
-        'targetDate' => '2015年2月8日',
-        'title' => '最近の練習風景',
-      ],
-      [
-      'targetDate' => '2015年1月3日',
-      'title' => '今年もよろしくお願いいたします',
-      ],
-    ];
+    //Get EntityManager
+    $em = $this->getDoctrine()->getManager();
+    //Get Entity repository
+    $blogArticleRepository = $em->getRepository('AppBundle:BlogArticle');
+    //Get data from DB
+    $blogList = $blogArticleRepository->findBy([], ['targetDate' => 'DESC']);
+
+    // $blogList = [
+    //   [
+    //     'targetDate' => '2015年3月15日',
+    //     'title' => '東京公演レポート',
+    //   ],
+    //   [
+    //     'targetDate' => '2015年2月8日',
+    //     'title' => '最近の練習風景',
+    //   ],
+    //   [
+    //   'targetDate' => '2015年1月3日',
+    //   'title' => '今年もよろしくお願いいたします',
+    //   ],
+    // ];
 
     return $this->render('Blog/latestList.html.twig', ['blogList' => $blogList]);
   }
--- src/AppBundle/Controller/InquiryController.php
+++ src/AppBundle/Controller/InquiryController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Inquiry;
 
 /**
 * @Route("/inquiry")
@@ -26,15 +27,26 @@ class InquiryController extends Controller
    */
   public function indexPostAction(Request $request)
   {
+      //Get difinition of Form
       $form = $this->createInquiryForm();
+      //Import sending data to form
       $form->handleRequest($request);
       if ($form->isValid())
       {
-          $data = $form->getData();
+          //Get Entity Data from form
+          $inquiry = $form->getData();
+
+          //Get EntityManager
+          $em = $this->getDoctrine()->getManager();
+          //Doctrine manage Entity
+          $em->persist($inquiry);
+          //Reflect on DB
+          $em->flush();
+
           $message = \Swift_Message::newInstance()->setSubject('Webサイトからのお知らせ')
                                                   ->setFrom('webmaster@exmaple.com')
                                                   ->setTo('admin@example.com')
-                                                  ->setBody($this->render('mail/inquiry.txt.twig', ['data' => $data]));
+                                                  ->setBody($this->render('mail/inquiry.txt.twig', ['data' => $inquiry]));
           $this->get('mailer')->send($message);
           return $this->redirect($this->generateUrl('app_inquiry_complete'));
       }
@@ -47,14 +59,14 @@ class InquiryController extends Controller
   /**
    * @Route("/complete")
    */
-  public function completeAction() 
+  public function completeAction()
   {
     return($this->render('Inquiry/complete.html.twig'));
   }
-  
+
   private function createInquiryForm()
   {
-      return $this->createFormBuilder()
+      return $this->createFormBuilder(new Inquiry())
       ->add('name', 'text')
       ->add('email', 'text')
       ->add('tel', 'text', [
